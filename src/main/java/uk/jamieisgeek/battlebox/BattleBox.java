b44b9d362294e391ec982bbbfce01b503034f603
JamieOnSootMC <jamie@sootmc.net>
2023-05-21 16:20:29 +0100

    init

---
+++ src/main/java/uk/jamieisgeek/battlebox/BattleBox.java
@@ -0,0 +1,17 @@
+package uk.jamieisgeek.battlebox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BattleBox extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
