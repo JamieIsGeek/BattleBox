6016d20886ef341655988bab6e68a27816422fb3
JamieOnSootMC <jamie@sootmc.net>
2023-06-09 13:38:41 +0100

    cleanup and add armor to archer kit

--- src/test/java/uk/jamieisgeek/battlebox/BattleBoxTest.java
+++ src/test/java/uk/jamieisgeek/battlebox/BattleBoxTest.java
@@ -1,28 +1,4 @@
-package uk.jamieisgeek.battlebox;
-
-import be.seeseemelk.mockbukkit.MockBukkit;
-import be.seeseemelk.mockbukkit.ServerMock;
-import org.junit.jupiter.api.*;
-import uk.jamieisgeek.battlebox.Game.State.State;
-
+import static org.junit.jupiter.api.Assertions.*;
 class BattleBoxTest {
-    private ServerMock server;
-    private BattleBox plugin;
-
-    @BeforeEach
-    public void setUp() {
-        server = MockBukkit.mock();
-        plugin = MockBukkit.load(BattleBox.class);
-    }
-
-    @AfterEach
-    public void tearDown() {
-        MockBukkit.unmock();
-    }
-
-    @Test
-    void setState() {
-        plugin.getGameState().setState(State.IN_PROGRESS);
-        assert plugin.getGameState().isState(State.IN_PROGRESS);
-    }
+  
 }
